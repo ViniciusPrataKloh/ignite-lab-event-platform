355aca470f9cdd9909d950491f629e3d281960bf
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2022-06-22 19:37:10 -0300

    Define components | Finish Header, Logo

---
+++ src/pages/Event.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/header/Header";
+import { Lesson } from "../components/lesson/Lesson";
+
+export function Event() {
+    return (
+        <div>
+            <Header />
+            <Lesson />
+        </div>
+    );
+}
