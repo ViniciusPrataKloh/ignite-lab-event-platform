440bb8c9d35cb61f9d41f518e62422111c0f2ea6
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2022-06-25 11:31:48 -0300

    Finish Sidebar | Lesson

--- src/components/lesson/Lesson.tsx
+++ src/components/lesson/Lesson.tsx
@@ -1,5 +1,54 @@
-export function Lesson() {
+import { CheckCircle, Lock } from "phosphor-react";
+
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+    const isLessonAvailable = new Date(props.availableAt) < new Date();
+
+    const availableAtFormated = new Intl.DateTimeFormat('pt-BR', {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false,
+        timeZone: 'America/Sao_Paulo',
+    }).format(new Date(props.availableAt))
+
     return (
-        <h1>Lesson</h1>
+        <a href="#">
+            <span className="text-gray-300">
+                {availableAtFormated}
+            </span>
+
+            <div className="rounded border border-gray-500 p-4 mt-2 ">
+                <header className="flex items-center justify-between">
+
+                    {isLessonAvailable
+                        ? (<span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
+                            <CheckCircle size={20} />
+                            Conteúdo liberado
+                        </span>)
+                        : (<span className="flex items-center gap-2 text-sm text-orange-500 font-medium">
+                            <Lock size={20} />
+                            Em breve
+                        </span>)
+                    }
+
+                    <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">
+                        {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
+                    </span>
+                </header>
+
+                <strong className="text-gray-200 mt-5 block">
+                    {props.title}
+                </strong>
+            </div>
+        </a>
     );
 }
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,11 +1,17 @@
 import { Header } from "../components/header/Header";
 import { Lesson } from "../components/lesson/Lesson";
+import { Player } from "../components/player/Player";
+import { Sidebar } from "../components/sidebar/sidebar";
 
 export function Event() {
     return (
-        <div>
+        <div className="flex flex-col min-h-screen">
             <Header />
-            <Lesson />
+
+            <main className="flex flex-1 ">
+                <Player />
+                <Sidebar />
+            </main>
         </div>
     );
 }
