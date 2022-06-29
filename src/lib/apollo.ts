abeccc7b5cac2b711d4ead8cbec884ecedb4dd21
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2022-06-29 11:25:47 -0300

    Add environment variables

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o0n6k101q101xmdpu4dgrl/master',
+    uri: import.meta.env.VITE_API_ACCESS_URL,
+    headers: {
+        'Auhtorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 });
